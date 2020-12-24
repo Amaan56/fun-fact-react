03bf9a62c7571bb50645008603d05ae2be8d922e
Amaan56 <shaikhamaan257@gmail.com>
2020-12-24 12:09:54 +0530

    feat: added UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <h1>Fun Fact in JS</h1>
+      </nav>
+      <main>
+        <section class="fact-section">
+          <p>Enter your birth year</p>
+          <input
+            class="input-section"
+            type="number"
+            placeholder="Your birth year"
+          />
+          <button class="fact-button">Get a fun fact!</button>
+          <div class="output-section-1 output-section"></div>
+          <br />
+          <div class="output-section-2 output-section"></div>
+        </section>
+      </main>
     </div>
   );
 }
