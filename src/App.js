4c54bd4330293a1bfbce077a511555639c74fd9b
Amaan56 <shaikhamaan257@gmail.com>
2020-12-24 19:49:33 +0530

    styles: added styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
+import { Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <nav>
         <h1>Fun Fact in JS</h1>
       </nav>
       <main>
-        <section class="fact-section">
+        <section className="fact-section">
           <p>Enter your birth year</p>
           <input
-            class="input-section"
+            className="input-section"
             type="number"
             placeholder="Your birth year"
           />
-          <button class="fact-button">Get a fun fact!</button>
-          <div class="output-section-1 output-section"></div>
-          <br />
-          <div class="output-section-2 output-section"></div>
+          <button className="fact-button">Get a fun fact!</button>
+          <p className="output-section"></p>
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
